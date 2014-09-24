5b2a00e4952e1589d624b82b35ccc4006a219047
Carl Pacey <Carl.Pacey@d2l.com>
2014-09-24 15:43:38 -0400

    Example change to showcase apporacle versions

--- src/app.jsx
+++ src/app.jsx
@@ -5,14 +5,16 @@ var React = require('react'),
 
 var Welcome = React.createClass({
 	render: function() {
-		return <div>Hello {this.props.firstName} {this.props.lastName}, 
-		you are in org-unit <b>{orgUnit.OrgUnitId}</b>.</div>;
+		return <div>
+			<div>Hello {this.props.firstName} {this.props.lastName}!</div>
+			<div>Welcome to org-unit <b>{orgUnit.OrgUnitId}</b>.</div>
+		</div>;
 	}
 });
 
 module.exports = function(node) {
 	React.renderComponent(
-		<Welcome firstName="Steve" lastName="McQueen"/>,
+		<Welcome firstName="Marie" lastName="Curie"/>,
 		node
 	);
 };
